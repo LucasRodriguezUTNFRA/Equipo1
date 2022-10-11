7c3b5b1fc01a5fd105cd6f5a42be3ba7384ae56c
maurnl <mauronluciano@hotmail.com>
2022-10-11 12:36:33 -0300

    Refactorizo test unitario en dos tests

--- Ticketek/TestTicketekEquipo1/AdministracionShould.cs
+++ Ticketek/TestTicketekEquipo1/AdministracionShould.cs
@@ -40,23 +40,18 @@ namespace TestTicketekEquipo1
         }
 
         [TestMethod]
-        [DataRow(Ubicacion.Campo, Categoria.Vip, 1000, 3500)]
-        [DataRow(Ubicacion.Campo, Categoria.Gold, 500, 2500)]
-        [DataRow(Ubicacion.Platea, Categoria.Gold, 1500, 4500)]
-        [DataRow(Ubicacion.Campo, Categoria.Clasic, 2000, 3500)]
-        public void DevolverUnaEntradaValida(Ubicacion ubicacion, Categoria categoria, float precioBase, float precioEsperado)
+        public void DevolverUnaEntradaValida()
         {
             // Given - Arrange
             Cliente cliente = new Cliente("Nombre", "Apellido", 12345678, DateTime.Parse("15/05/1980"));
             Artista artista = new Artista("Luis", "Miguel", 10000000, DateTime.Parse("19/04/1970"), "Luismi");
-            Show show = new Show("Luna Park", artista, DateTime.Now + TimeSpan.FromDays(3), 10, precioBase);
+            Show show = new Show("Luna Park", artista, DateTime.Now + TimeSpan.FromDays(3), 10, 1000);
             // When - Act
-            Entrada entrada = Administracion.AgregarEspectadorAlShow(cliente, show, categoria, ubicacion);
+            Entrada entrada = Administracion.AgregarEspectadorAlShow(cliente, show, Categoria.Gold, Ubicacion.Campo);
             // Then - Assert
             Assert.IsNotNull(entrada);
             Assert.AreEqual(show, entrada.Show);
             Assert.AreEqual(cliente, entrada.Cliente);
-            Assert.AreEqual(precioEsperado, entrada.PrecioTotalEnPesos);
         }
 
 
---
+++ Ticketek/TestTicketekEquipo1/EntradaShould.cs
@@ -0,0 +1,25 @@
+﻿using Boleteria;
+
+namespace TestTicketekEquipo1
+{
+    [TestClass]
+    public  class EntradaShould
+    {
+        [TestMethod]
+        [DataRow(Ubicacion.Campo, Categoria.Vip, 1000, 3500)]
+        [DataRow(Ubicacion.Campo, Categoria.Gold, 500, 2500)]
+        [DataRow(Ubicacion.Platea, Categoria.Gold, 1500, 4500)]
+        [DataRow(Ubicacion.Campo, Categoria.Clasic, 2000, 3500)]
+        public void CalcularBienElPrecio(Categoria categoria, Ubicacion ubicacion, float precioBase, float precioEsperado)
+        {
+            // Given
+            Cliente cliente = new Cliente("Nombre", "Apellido", 12345678, DateTime.Parse("15/05/1980"));
+            Artista artista = new Artista("Luis", "Miguel", 10000000, DateTime.Parse("19/04/1970"), "Luismi");
+            Show show = new Show("Luna Park", artista, DateTime.Now + TimeSpan.FromDays(3), 10, precioBase);
+            // When
+            Entrada entrada = new Entrada(show, cliente, categoria, ubicacion);
+            // Then
+            Assert.AreEqual(precioEsperado, entrada.PrecioTotalEnPesos);
+        }
+    }
+}
